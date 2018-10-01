db29ed5893efda75609e3b6a5cd79477ad23c0f9
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2018-10-01 10:43:29 -0500

    get_downloads

--- modelsof.py
+++ modelsof.py
@@ -1,11 +1,24 @@
 import csv
-import os.path
+import os, os.path
 import sys
 
 from bs4 import BeautifulSoup
 import requests
 
 base_url = 'https://dataverse.harvard.edu'
+
+def attempt(url):
+    attempts = 0
+    while True:
+        try:
+            r = requests.get(url, timeout=10)
+            print(r.url)
+            return r
+        except:
+            if attempts < 4:
+                attempts += 1
+                continue
+            raise
 
 def get_datasets(dataverse):
     fname = dataverse + '.csv'
@@ -15,8 +28,7 @@ def get_datasets(dataverse):
     url = '/'.join([base_url, 'dataverse', dataverse])
     datasets = []
     while True:
-        r = requests.get(url, timeout=10)
-        print(r.url)
+        r = attempt(url)
         soup = BeautifulSoup(r.text, 'html.parser')
         for ds in soup.find_all(class_='datasetResult'):
             datasets.append(dict(title=ds.a.span.text, href=ds.a['href']))
@@ -42,18 +54,7 @@ def get_files(dataverse):
     with open(dataverse + '.csv') as f:
         r = csv.DictReader(f)
         for row in r:
-            attempts = 0
-            while True:
-                try:
-                    r = requests.get(base_url + row['href'], timeout=10)
-                    print(r.url)
-                    break
-                except:
-                    if attempts < 4:
-                        attempts += 1
-                        continue
-                    raise
-
+            r = attempt(base_url + row['href'])
             soup = BeautifulSoup(r.text, 'html.parser')
             for md in soup.find_all('td', class_='col-file-metadata'):
                 files.append(dict(row, filename=md.a.text.strip(), file_href=md.a['href']))
@@ -63,6 +64,24 @@ def get_files(dataverse):
         w.writeheader()
         w.writerows(files)
 
-#url = base_url + '/api/access/datafile/:persistentId?'
-cmd = {'get_datasets': get_datasets, 'get_files': get_files}[sys.argv[1]]
+def get_downloads(dataverse):
+    get_files(dataverse)
+    with open(dataverse + '_files.csv') as f:
+        r = csv.DictReader(f)
+        for row in r:
+            id = row['file_href'].split('?')[-1].split('&')[0]
+            dir = dataverse + '/' + id.split('/')[2]
+            os.makedirs(dir, exist_ok=True)
+            fname = dir + '/' + row['filename']
+            if os.path.isfile(fname):
+                continue
+
+            r = attempt(base_url + '/api/access/datafile/:persistentId?' + id)
+            with open(fname, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=128):
+                    f.write(chunk)
+
+cmd = {'get_datasets': get_datasets, 
+       'get_files': get_files,
+       'get_downloads': get_downloads}[sys.argv[1]]
 cmd(sys.argv[2])
