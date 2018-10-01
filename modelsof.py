6126330a2508bcf583062d2e89c386a4bb5ea2a2
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2018-10-01 08:39:21 -0500

    get_files

--- modelsof.py
+++ modelsof.py
@@ -1,29 +1,68 @@
 import csv
+import os.path
 import sys
 
 from bs4 import BeautifulSoup
 import requests
 
-dataverse = sys.argv[1]
 base_url = 'https://dataverse.harvard.edu'
-url = '/'.join([base_url, 'dataverse', dataverse])
 
-datasets = []
-while True:
-    r = requests.get(url)
-    print(r.url)
+def get_datasets(dataverse):
+    fname = dataverse + '.csv'
+    if os.path.isfile(fname):
+        return print(fname + ' already exists')
 
-    soup = BeautifulSoup(r.text, 'html.parser')
-    for ds in soup.find_all(class_='datasetResult'):
-        datasets.append(dict(title=ds.a.span.text, href=ds.a['href']))
+    url = '/'.join([base_url, 'dataverse', dataverse])
+    datasets = []
+    while True:
+        r = requests.get(url, timeout=10)
+        print(r.url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        for ds in soup.find_all(class_='datasetResult'):
+            datasets.append(dict(title=ds.a.span.text, href=ds.a['href']))
 
-    next_page = soup.find('a', text='Next >')
-    if next_page['href'].split('=')[-1] == url.split('=')[-1]:
-        break
+        next_page = soup.find('a', text='Next >')
+        if next_page['href'].split('=')[-1] == url.split('=')[-1]:
+            break
 
-    url = base_url + next_page['href']
+        url = base_url + next_page['href']
 
-with open(dataverse + '.csv', 'w') as csv_f:
-    w = csv.DictWriter(csv_f, fieldnames=['title', 'href'])
-    w.writeheader()
-    w.writerows(datasets)
+    with open(fname, 'w') as f:
+        w = csv.DictWriter(f, ['title', 'href'])
+        w.writeheader()
+        w.writerows(datasets)
+
+def get_files(dataverse):
+    get_datasets(dataverse)
+    fname = dataverse + '_files.csv'
+    if os.path.isfile(fname):
+        return print(fname + ' already exists')
+
+    files = []
+    with open(dataverse + '.csv') as f:
+        r = csv.DictReader(f)
+        for row in r:
+            attempts = 0
+            while True:
+                try:
+                    r = requests.get(base_url + row['href'], timeout=10)
+                    print(r.url)
+                    break
+                except:
+                    if attempts < 4:
+                        attempts += 1
+                        continue
+                    raise
+
+            soup = BeautifulSoup(r.text, 'html.parser')
+            for md in soup.find_all('td', class_='col-file-metadata'):
+                files.append(dict(row, filename=md.a.text.strip(), file_href=md.a['href']))
+
+    with open(fname, 'w') as f:
+        w = csv.DictWriter(f, ['title', 'href', 'filename', 'file_href'])
+        w.writeheader()
+        w.writerows(files)
+
+#url = base_url + '/api/access/datafile/:persistentId?'
+cmd = {'get_datasets': get_datasets, 'get_files': get_files}[sys.argv[1]]
+cmd(sys.argv[2])
