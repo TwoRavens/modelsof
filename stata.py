fefcc43bb3fe23e87c70bcbaad283e8bf92bc922
aaron <you@example.com>
2019-01-05 04:07:25 -0600

    lexer works on all ajps

--- stata.py
+++ stata.py
@@ -1,4 +1,5 @@
 import collections
+import glob
 import json
 import os, os.path
 import sys
@@ -6,10 +7,11 @@ import sys
 Token = collections.namedtuple('Token', 'type value line column')
 
 class InputError(Exception):
-    def __init__(self, char, line, column):
+    def __init__(self, char, line, column, text):
         self.char = char
         self.line = line
         self.column = column
+        self.text = text 
 
 class Lexer:
     def __init__(self, input):
@@ -19,6 +21,7 @@ class Lexer:
         self.ch = '\0' 
         self.line = 1
         self.column = 0
+        self.last_token = None
         self.read_char()
 
     def __iter__(self):
@@ -30,74 +33,91 @@ class Lexer:
         if ch == '\0':
             raise StopIteration
 
-        if ch in '\n\\,(){}[]<>|&+-:%':
+        if ch in "\n\\,(){}[]<>|&+-:%;'^" or ord(ch) > 127:
             tok = self.token() 
         elif ch == '/':
             tok = self.token() 
             if self.peek_char() == '*':
-                pos, line, col = self.position, self.line, self.column
+                pos, line, col = self.get_current()
                 while self.input[self.position - 1:self.position + 1] != '*/':
                     self.read_char()
+                    if self.ch == '\0':
+                        raise InputError(ch, line, col, self.input[pos - col + 1:pos + 10]) 
                 tok = self.token('comment', self.input[pos:self.position + 1], line, col) 
             elif self.peek_char() == '/':
-                pos, line, col = self.position, self.line, self.column
+                pos, line, col = self.get_current()
                 while True:
                     self.read_char()
                     if self.ch == '\n' or self.ch == '\0':
                         break
-                tok = self.token('///', self.input[pos:self.position], line, col) 
+                tok = self.token('///', self.input[pos:self.position], line, col)
+        elif ch == '~':
+            tok = self.token() 
+            if self.peek_char() == '=':
+                pos, line, col = self.get_current()
+                self.read_char()
+                tok = self.token('~=', self.input[pos:self.position + 1], line, col) 
         elif ch == '!':
             tok = self.token() 
             if self.peek_char() == '=':
-                pos, line, col = self.position, self.line, self.column
+                pos, line, col = self.get_current()
                 self.read_char()
                 tok = self.token('!=', self.input[pos:self.position + 1], line, col) 
         elif ch == '=':
             tok = self.token() 
             if self.peek_char() == '=':
-                pos, line, col = self.position, self.line, self.column
+                pos, line, col = self.get_current()
                 self.read_char()
                 tok = self.token('==', self.input[pos:self.position + 1], line, col) 
         elif ch == '*':
             tok = self.token() 
-            if self.column == 1:
-                pos, line, col = self.position, self.line, self.column
+            if self.last_token and self.last_token.type == '\n':
+                pos, line, col = self.get_current()
                 while self.peek_char() != '\n':
                     self.read_char()
                 tok = self.token('comment', self.input[pos:self.position + 1], line, col) 
         elif ch == '#':
-            pos, line, col = self.position, self.line, self.column
+            pos, line, col = self.get_current()
             while self.peek_char() != '\n':
                 self.read_char()
             tok = self.token('comment', self.input[pos:self.position + 1], line, col) 
         elif ch == '"':
-            pos, line, col = self.position + 1, self.line, self.column
+            pos, line, col = self.get_current()
+            pos += 1
             while True:
                 self.read_char()
                 if self.ch == '"' or self.ch == '\0':
                     break
             tok = self.token('string', self.input[pos:self.position], line, col) 
         elif ch == '`':
-            pos, line, col = self.position + 1, self.line, self.column
-            while True:
-                self.read_char()
-                if self.ch == "'" or self.ch == '\0':
-                    break
-            tok = self.token("`'", self.input[pos:self.position], line, col) 
+            if self.peek_char() == '"':
+                pos, line, col = self.get_current()
+                while self.ch != '\0' and self.input[self.position - 1:self.position + 1] != '"\'':
+                    self.read_char()
+                tok = self.token('string', self.input[pos:self.position + 1], line, col) 
+            else:
+                pos, line, col = self.get_current()
+                pos += 1
+                while True:
+                    self.read_char()
+                    if self.ch == "'" or self.ch == '\0':
+                        break
+                tok = self.token("`'", self.input[pos:self.position], line, col) 
         elif self.is_letter():
-            pos, line, col = self.position, self.line, self.column
-            while self.is_letter(first=False):
+            pos, line, col = self.get_current()
+            while self.is_letter() or self.ch == '.' or '0' <= self.ch <= '9':
                 self.read_char()
             return self.token('identifier', self.input[pos:self.position], line, col) 
         elif self.is_digit():
-            pos, line, col = self.position, self.line, self.column
+            pos, line, col = self.get_current()
             while self.is_digit(first=False):
                 self.read_char()
             return self.token('number', self.input[pos:self.position], line, col) 
         else:
-            raise InputError(ch, self.line, self.column) 
+            raise InputError(ch, self.line, self.column, self.input[self.position - self.column + 1:self.position + 10]) 
 
         self.read_char()
+        self.last_token = tok
         return tok
 
     def skip_whitespace(self):
@@ -117,21 +137,27 @@ class Lexer:
     def peek_char(self):
         return '\0' if self.read_position >= len(self.input) else self.input[self.read_position]
 
-    def is_letter(self, first=True):
-        ch = self.ch
-        return 'A' <= ch <= 'Z' or 'a' <= ch <= 'z' or ch in '_' or not first and (ch in '.' or '0' <= ch <= '9')
+    def get_current(self):
+        return self.position, self.line, self.column
+
+    def is_letter(self):
+        return 'A' <= self.ch <= 'Z' or 'a' <= self.ch <= 'z' or self.ch in '@_$'
 
     def is_digit(self, first=True):
         ch = self.ch
         return '0' <= ch <= '9' or ch in '.' or not first and 'a' <= ch <= 'z'
 
     def token(self, type='', value='', line=0, column=0):
-         return Token(type or self.ch, value or self.ch, line or self.line, column or self.column) 
+        return Token(type or self.ch, value or self.ch, line or self.line, column or self.column) 
 
-if __name__ == '__main__':
-    file = sys.argv[1]
-    with open(file) as f:
-        lexer = Lexer(f.read())
+def run(file):
+    print(file)
+    try:
+        with open(file) as f:
+            lexer = Lexer(f.read())
+    except UnicodeError:
+        with open(file, encoding='cp1252') as f:
+            lexer = Lexer(f.read())
 
     lines, line = [], []
     for tok in lexer:
@@ -139,8 +165,15 @@ if __name__ == '__main__':
             line and lines.append(line)
             line = []
         else:
-            line.append(dict(type=tok.type, value=tok.value, line=tok.line, column=tok.column))
+            tok1 = dict(type=tok.type, line=tok.line, column=tok.column)
+            if tok.type != tok.value:
+                tok1['value'] = tok.value
+            line.append(tok1)
 
     os.makedirs('out/' + os.path.dirname(file), exist_ok=True)
     with open(f'out/{file}.json', 'w') as f:
-        json.dump(lines, f, indent=2)
+       json.dump(lines, f, indent=2)
+
+if __name__ == '__main__':
+    for file in glob.glob(sys.argv[1], recursive=True):
+        run(file)
